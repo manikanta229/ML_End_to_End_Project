cbd94a85fbde4830fffb7e994026e66a6294ea6d
Manikanta Alluri <35761023+manikanta229@users.noreply.github.com>
2024-01-05 14:56:22 -0600

    error solve

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,7 +14,7 @@ from dataclasses import dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv")
     test_data_path: str = os.path.join('artifacts',"test.csv")
-    raw_data_path_data_path: str = os.path.join('artifacts',"data.csv")
+    raw_data_path: str = os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
