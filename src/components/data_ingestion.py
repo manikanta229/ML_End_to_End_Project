1eda513b8cf4066bacd6f9ac30299872c4574a3f
Manikanta Alluri <35761023+manikanta229@users.noreply.github.com>
2024-01-08 14:50:15 -0600

    done data trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,11 @@ from src.components.data_transformation import DataTransformationConfig
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-#from src.components.model_trainer import ModelTrainerConfig
-#from src.components.model_trainer import ModelTrainer
+
+
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 #Decorators provide a clean and readable way to extend or modify the behavior of functions or classes. 
 #They are a powerful feature in Python often used in frameworks, libraries, and other advanced programming patterns.
@@ -66,7 +69,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    #modeltrainer=ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
